cda39757c1fe99556b6497f9956faa53525713aa
kapibala <1091617298@qq.com>
2025-06-20 19:56:38 +0800

    项目初始化

---
+++ src/main/java/com/kapibala/offercat/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.kapibala.offercat.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kapibala.offercat.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+*
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/kapibala/offercat/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,29 @@
+package com.kapibala.offercat.model.dto.postfavour;
+
+import com.kapibala.offercat.common.PageRequest;
+import com.kapibala.offercat.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+*
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
