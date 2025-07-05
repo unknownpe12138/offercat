daac01560b18bd978b7a6116daeb508aae748e4c
kapibala <1091617298@qq.com>
2025-07-05 20:39:48 +0800

    编写questionEsDao,测试两种Spring Data Elasticsearch的使用方法.1：spring默认提供的es客户端对象，elasticsearchresttemplate（crud更加灵活，适用于更加复杂的操作）2：elasticsearchrepository（默认提供更简单易用的crud，返回结果更直接）

---
+++ src/main/java/com/kapibala/offercat/esdao/QuestionEsDao.java
@@ -0,0 +1,15 @@
+package com.kapibala.offercat.esdao;
+
+import com.kapibala.offercat.model.dto.question.QuestionEsDTO;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+/**
+ * 题目 ES 操作
+ *
+ */
+public interface QuestionEsDao extends ElasticsearchRepository<QuestionEsDTO, Long> {
+
+    List<QuestionEsDTO> findByUserId(Long userId);
+}
